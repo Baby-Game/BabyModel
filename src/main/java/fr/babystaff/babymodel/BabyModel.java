51d0b9b9fae33b50569ac8846b4188447226d5a8
Louis292 <105144467+Louis292@users.noreply.github.com>
2024-11-27 11:23:51 +0100

    Remove LuckPerms and ProtocolLib

--- src/main/java/fr/babystaff/babymodel/BabyModel.java
+++ src/main/java/fr/babystaff/babymodel/BabyModel.java
@@ -1,17 +1,11 @@
 package fr.babystaff.babymodel;
 
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
 import fr.babystaff.babymodel.actionBar.ActionBarManager;
 import fr.babystaff.babymodel.dataBase.DataBaseManager;
 import fr.babystaff.babymodel.hologram.HologramManager;
-import net.luckperms.api.LuckPerms;
-import net.luckperms.api.LuckPermsProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BabyModel extends JavaPlugin {
-    private LuckPerms luckPerms;
-    private ProtocolManager protocolManager;
 
     private DataBaseManager dataBaseManager;
 
@@ -20,8 +14,6 @@ public final class BabyModel extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        loadLuckPerms();
-        loadProtocolLib();
 
         this.dataBaseManager = new DataBaseManager();
 
@@ -34,35 +26,6 @@ public final class BabyModel extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-    }
-
-    public void loadProtocolLib() {
-        if (getServer().getPluginManager().getPlugin("ProtocolLib") == null) {
-            getLogger().severe("ProtocolLib n'est pas installé !");
-            return;
-        }
-
-        protocolManager = ProtocolLibrary.getProtocolManager();
-        getLogger().info("ProtocolLib est prêt à être utilisé !");
-    }
-
-    public void loadLuckPerms() {
-        luckPerms = LuckPermsProvider.get();
-
-        if (luckPerms == null) {
-            getLogger().severe("LuckPerms API n'a pas pu être initialisé !");
-            return;
-        }
-
-        getLogger().info("LuckPerms API est prêt à être utilisé !");
-    }
-
-    public LuckPerms getLuckPerms() {
-        return luckPerms;
-    }
-
-    public ProtocolManager getProtocolManager() {
-        return protocolManager;
     }
 
     public ActionBarManager getActionBarManager() {
