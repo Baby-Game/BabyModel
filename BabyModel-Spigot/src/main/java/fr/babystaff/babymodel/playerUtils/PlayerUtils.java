53786b1c789e35ad1ad201f34e0817eed73715d3
Louis292 <105144467+Louis292@users.noreply.github.com>
2025-02-09 16:13:05 +0100

    Update PlayerUtils.java

--- BabyModel-Spigot/src/main/java/fr/babystaff/babymodel/playerUtils/PlayerUtils.java
+++ BabyModel-Spigot/src/main/java/fr/babystaff/babymodel/playerUtils/PlayerUtils.java
@@ -1,11 +1,27 @@
 package fr.babystaff.babymodel.playerUtils;
 
+import fr.babystaff.babymodel.armorStand.ArmorStand;
+import fr.babystaff.babymodel.hologram.Hologram;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class PlayerUtils {
 
+    // Méthode pour obtenir le ping d'un joueur
     public int getPing(Player player) {
         return ((CraftPlayer) player).getHandle().ping;
+    }
+
+    // Méthode pour associer un hologramme avec un joueur (ajouter un ArmorStand en passager)
+    public void partnerHologramWithPlayer(Player player, Hologram hologram) {
+        // Vérifie que l'hologramme a des ArmorStands
+        if (!hologram.getArmorStands().isEmpty()) {
+            // Récupère le premier ArmorStand de l'hologramme
+            ArmorStand firstArmorStand = hologram.getFirstEntity();
+            if (firstArmorStand != null) {
+                // Ajoute l'ArmorStand comme passager du joueur
+                player.setPassenger(firstArmorStand.getArmorStand());
+            }
+        }
     }
 }
